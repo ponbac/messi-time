781e96a3f9843773415954b54d4af5e5904fb028
ponbac <root@ponbac.xyz>
2022-06-02 12:46:08 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#53479B",
+        secondary: "#BE6391",
+        neutral: "#E6E9FF",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
