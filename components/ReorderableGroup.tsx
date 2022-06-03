a55a5024d0a673f87f0b03e4136826aebebab17d
ponbac <root@ponbac.xyz>
2022-06-03 16:39:20 +0200

    decent groups

--- components/ReorderableGroup.tsx
+++ components/ReorderableGroup.tsx
@@ -3,8 +3,15 @@ import { useState } from "react";
 
 const GroupItem: React.FC<{ team: Team }> = ({ team }) => {
   return (
-    <div className="bg-primary w-36 h-15">
-      <p>{team.name}</p>
+    <div className="w-44 hover:cursor-pointer hover:bg-primary/30 transition-all mx-2 flex flex-row items-center font-mono bg-gray-400/30 backdrop-blur-sm py-2 px-4 rounded-lg">
+      {/* <img
+        className="rounded-full p-1 ring-2 hover:ring-4 transition-all ring-primary"
+        src={player.avatar}
+        alt={`${player.name} avatar`}
+        width={70}
+        height={70}
+      /> */}
+      <h1>{team.name}</h1>
     </div>
   );
 };
@@ -13,23 +20,21 @@ const ReorderableGroup: React.FC<{ group: Group }> = ({ group }) => {
   const [groupItem, setGroup] = useState<Team[]>(group.teams);
 
   return (
-    // <div className="space-y-1">
-    //   {group.map((team, index) => {
-    //     return ;
-    //   })}
-    // </div>
-    <Reorder.Group
-      axis="y"
-      values={groupItem}
-      onReorder={setGroup}
-      className="space-y-1"
-    >
-      {groupItem.map((item) => (
-        <Reorder.Item key={item.id} value={item}>
-          <GroupItem key={item.name} team={item} />
-        </Reorder.Item>
-      ))}
-    </Reorder.Group>
+    <div className="flex flex-col items-center">
+        <p className="text-xl font-bold font-mono">Group {group.id}</p>
+      <Reorder.Group
+        axis="y"
+        values={groupItem}
+        onReorder={setGroup}
+        className="pt-2 space-y-1"
+      >
+        {groupItem.map((item) => (
+          <Reorder.Item key={item.id} value={item}>
+            <GroupItem key={item.name} team={item} />
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
+    </div>
   );
 };
 
--- pages/bet.tsx
+++ pages/bet.tsx
@@ -1,6 +1,6 @@
 import ReorderableGroup from "../components/ReorderableGroup";
 
-const exampleGroup = {
+const exampleGroupA = {
   id: "A",
   teams: [
     { id: "1", name: "Sweden" },
@@ -10,10 +10,43 @@ const exampleGroup = {
   ],
 };
 
+const exampleGroupB = {
+    id: "B",
+    teams: [
+      { id: "5", name: "Brazil" },
+      { id: "6", name: "England" },
+      { id: "7", name: "Belgium" },
+      { id: "8", name: "North Korea" },
+    ],
+  };
+
+  const exampleGroupC = {
+    id: "C",
+    teams: [
+      { id: "9", name: "Italy" },
+      { id: "10", name: "Wales" },
+      { id: "11", name: "Peru" },
+      { id: "12", name: "USA" },
+    ],
+  };
+
+  const exampleGroupD = {
+    id: "D",
+    teams: [
+      { id: "13", name: "Canada" },
+      { id: "14", name: "Mexico" },
+      { id: "15", name: "Spain" },
+      { id: "16", name: "Japan" },
+    ],
+  };
+
 const Betting: React.FC<{}> = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <ReorderableGroup group={exampleGroup} />
+    <div className="min-h-screen flex flex-row justify-center items-center gap-8">
+      <ReorderableGroup group={exampleGroupA} />
+      <ReorderableGroup group={exampleGroupB} />
+      <ReorderableGroup group={exampleGroupC} />
+      <ReorderableGroup group={exampleGroupD} />
     </div>
   );
 };
