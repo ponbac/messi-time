17aff5866e89c657266fd54c0365296be8a39b7b
ponbac <root@ponbac.xyz>
2022-06-07 00:27:39 +0200

    can now select team

--- pages/bet/match/[id].tsx
+++ pages/bet/match/[id].tsx
@@ -1,26 +1,45 @@
-import { match } from "assert";
 import { useRouter } from "next/router";
-import { FC } from "react";
+import React, { FC, useState } from "react";
 import TeamFlag from "../../../components/TeamFlag";
 
-const TeamBlock: FC<{ team: Team }> = ({ team }) => {
+const TeamBlock: FC<{
+  team: Team;
+  selected: boolean;
+  toggleSelectedTeam: (team: Team) => void;
+}> = ({ team, selected, toggleSelectedTeam }) => {
   const flagWidth = "4.5rem";
 
-  const handleClick = () => {
-    
-  }
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    toggleSelectedTeam(team);
+  };
 
   return (
-    <div className="flex flex-col items-center cursor-pointer" onClick={handleClick}>
+    <button
+      className={
+        "flex flex-col items-center w-52 p-4 rounded-xl " +
+        (selected == true ? "bg-green-600/40" : "")
+      }
+      onClick={handleClick}
+    >
       <p className="text-4xl pb-2">{team.name}</p>
       <TeamFlag team={team} width={flagWidth} />
-    </div>
+    </button>
   );
 };
 
 const PredictMatch: FC<{}> = ({}) => {
   const router = useRouter();
   const { id } = router.query;
+
+  const [selectedTeam, setSelectedTeam] = useState<Team>();
+  const teamClickHandler = (team: Team) => {
+    if (selectedTeam?.id == team.id) {
+      setSelectedTeam(undefined);
+    } else {
+      setSelectedTeam(team);
+    }
+  };
 
   const testMatch: Match = {
     id: "1",
@@ -33,12 +52,20 @@ const PredictMatch: FC<{}> = ({}) => {
 
   return (
     <div className="font-mono flex flex-row min-w-full min-h-screen items-center justify-center gap-12">
-      <TeamBlock team={testMatch.homeTeam} />
+      <TeamBlock
+        team={testMatch.homeTeam}
+        selected={testMatch.homeTeam.id == selectedTeam?.id}
+        toggleSelectedTeam={teamClickHandler}
+      />
       <div className="flex flex-col text-center">
         <p className="text-2xl">vs</p>
         <p className="text-sm italic">ID: {id}</p>
       </div>
-      <TeamBlock team={testMatch.awayTeam} />
+      <TeamBlock
+        team={testMatch.awayTeam}
+        selected={testMatch.awayTeam.id == selectedTeam?.id}
+        toggleSelectedTeam={teamClickHandler}
+      />
     </div>
   );
 };
