0b7647a8838d5b29aa3a80b8b272d0e1f32cc1e2
ponbac <ponbac@student.chalmers.se>
2022-06-09 22:50:50 +0200

    added redux wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { wrapper } from "../utils/store";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -10,4 +11,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
